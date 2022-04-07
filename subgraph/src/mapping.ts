fd9ef8d22ec8dc3143aed5991d42f9ecb99cf79c
Wojciech Rybakiewicz <wojtek.rybakiewicz@gmail.com>
2022-04-07 17:12:04 +0200

    update subgraph

--- subgraph/src/mapping.ts
+++ subgraph/src/mapping.ts
@@ -1,27 +1,16 @@
-import {Address, BigInt} from "@graphprotocol/graph-ts"
+import {BigInt} from "@graphprotocol/graph-ts"
 import {MemeEntity} from "../generated/schema"
 import {Transfer, VoteDown, VoteUp} from "../generated/MemeNFT/MemeNFT";
+import {MemeNFT} from "../generated/MemeNFT/MemeNFT";
 
 export function handleVoteUp(event: VoteUp): void {
-    let entity = MemeEntity.load(event.params.tokenId.toHex())
-
-    if (!entity) {
-        entity = new MemeEntity(event.params.tokenId.toHex())
-        entity.voteCount = BigInt.fromI32(0)
-    }
-
+    let entity = MemeEntity.load(event.params.tokenId.toHex())!
     entity.voteCount = entity.voteCount.plus(BigInt.fromI32(1))
     entity.save()
 }
 
 export function handleVoteDown(event: VoteDown): void {
-    let entity = MemeEntity.load(event.params.tokenId.toHex())
-
-    if (!entity) {
-        entity = new MemeEntity(event.params.tokenId.toHex())
-        entity.voteCount = BigInt.fromI32(0)
-    }
-
+    let entity = MemeEntity.load(event.params.tokenId.toHex())!
     entity.voteCount = entity.voteCount.minus(BigInt.fromI32(1))
     entity.save()
 }
@@ -29,8 +18,11 @@ export function handleVoteDown(event: VoteDown): void {
 export function handleMint(event: Transfer): void {
     let entity = MemeEntity.load(event.params.tokenId.toHex())
     if (!entity) {
-        entity = new MemeEntity(event.params.tokenId.toHex())
+        const memeNFT = MemeNFT.bind(event.address)
+        const tokenId = event.params.tokenId
+        entity = new MemeEntity(tokenId.toHex())
         entity.voteCount = BigInt.fromI32(0)
+        entity.link = memeNFT.tokenURI(tokenId)
         entity.save()
     }
 }
